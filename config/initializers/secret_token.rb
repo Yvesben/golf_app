6321319b2897cb883e24d9172da40471e42d921a
Benchimol Yves <yves.benchimol@polytechnique.org>
2014-02-07 09:47:33 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GolfApp::Application.config.secret_key_base = 'e62dd7297e7724d82b02a2ba852a7e882a349b5906995410297b8a604bf85237d3d5a639f4a86b94f94ce609a77d52b7c127026e0dd5aa3a2cd53020b4639a58'
